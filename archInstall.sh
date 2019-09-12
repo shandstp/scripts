336e46968a75ccd9b36c90c0e1ea1a479f27caf4
Travis Shands <shandstp@gmail.com>
2019-09-11 21:05:35 -0700

    adjusted chown to use arch-chroot

--- archInstall.sh
+++ archInstall.sh
@@ -117,7 +117,7 @@ echo "127.0.1.1	${hostname}.localdomain	${hostname}" >> /mnt/etc/hosts
 echo "GTK_IM_MODULE=fcitx" >> /mnt/home/${username}/.pam_environment
 echo "QT_IM_MODULE=fcitx" >> /mnt/home/${username}/.pam_environment
 echo "XMODIFIERS=@im=fcitx" >> /mnt/home/${username}/.pam_environment
-chown ${username} /mnt/home/${username}/.pam_environment
+arch-chroot /mnt chown ${username} /home/${username}/.pam_environment
 
 arch-chroot /mnt sed -i s/"#[multilib]"/"[multilib]"/ /etc/pacman.conf
 arch-chroot /mnt sed -i s/"#Include = /etc/pacman.d/mirrorlist"/"[Include = /etc/pacman.d/mirrorlist"/ /etc/pacman.conf
@@ -125,4 +125,5 @@ arch-chroot /mnt sed -i s/"#Include = /etc/pacman.d/mirrorlist"/"[Include = /etc
 arch-chroot /mnt pacman -Syu steam steam-native-runtime firefox lib32-nvidia-utils nvidia-settings
 
 echo "That should about do it. Rebooting..."
-reboot
+#reboot
+echo "Or not... Standing by"
